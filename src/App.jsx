eea1d101f00810bae0d795e6376ecc4b6b56a518
Michal Gornicki <gornickimichal@gmail.com>
2021-10-26 18:29:14 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="content">
+      Cleanedup React Appd
+    </div>
+  );
+}
+
+export default App;
